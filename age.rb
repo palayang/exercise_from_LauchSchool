ac772670aa7d13b6e50793d31f501ce57ae10cc4
Tim <palapala291295@gmail.com>
2016-05-27 17:22:39 +0800

    age.rb

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "how old are you?"
+age = gets.chomp
+
+puts "after 10 years you will be:"
+puts age.to_i + 10
+puts "after 20 years you will be:"
+puts age.to_i + 20
+puts "after 30 years you will be:"
+puts age.to_i + 30
+puts "after 40 years you will be:"
+puts age.to_i + 40
