6f977d6c335e5d878ca7639a6abf4bb515928c23
Tim <palapala291295@gmail.com>
2016-05-27 17:34:14 +0800

    change name.rb

--- name.rb
+++ name.rb
@@ -1,7 +1,14 @@
-puts "please input your name:"
-name = gets.chomp
+#puts "please input your name:"
+#name = gets.chomp
 #puts "welcome, #{name}"
-10.times do |n|
-	puts name
-end
 
+
+#10.times do |n|
+#	puts name
+#end
+
+puts "please input your first name:"
+first_name = gets.chomp
+puts "please input your last name:"
+last_name = gets.chomp
+puts first_name + " " + last_name
