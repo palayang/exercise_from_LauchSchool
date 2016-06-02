2cbaeff6476da0c31e6ecc9fe02ab0a196f7312f
Tim <palapala291295@gmail.com>
2016-06-02 10:47:08 +0800

    passing_block.rb add another method

--- passing_block.rb
+++ passing_block.rb
@@ -3,3 +3,12 @@ def pass_block(&b)
 end
 
 pass_block {puts "here"}
+
+def take_block(number, &block)
+  block.call(number)
+end
+
+
+take_block(42) do |num|
+  puts "Block being called in the method! #{num}"
+end
