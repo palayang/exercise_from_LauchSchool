862ef254512dd2f9178d52668a4d0a3a9e840336
Tim <palapala291295@gmail.com>
2016-06-04 00:25:58 +0800

    exercise

--- exception_example.rb
+++ exception_example.rb
@@ -8,5 +8,5 @@ a.each do |e|
 	end
 end
 
-#inline_exception_example
+#inline_exception_example, will stop when error happend
 a.each {|e| puts " name = #{e}, length = #{e.length}"} rescue puts "something wrong"
