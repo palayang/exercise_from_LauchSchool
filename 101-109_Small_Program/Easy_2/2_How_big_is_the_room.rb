f52e1ac9aabd774b27dfaa4fdcbcca4063d0a6cb
Tim <palapala291295@gmail.com>
2017-02-04 13:29:01 +0800

    101-109_small_problems_easy2_2_How_big_is_the_room.rb

--- 101-109_Small_Program/Easy_2/2_How_big_is_the_room.rb
+++ 101-109_Small_Program/Easy_2/2_How_big_is_the_room.rb
@@ -1,8 +1,8 @@
 puts "Enter the length of the room in meters:"
-length = gets.chomp.to_i
+length = gets.to_f
 puts "Enter the width of the room in meters:"
-width = gets.chomp.to_i
-puts "The area of the room is #{length*width} square meters (#{length*width*10.7639} square feet)."
+width = gets.to_f
+puts "The area of the room is #{(length*width).round(2)} square meters (#{(length*width*10.7639).round(2)} square feet)."
 
 
 # Enter the length of the room in meters:
